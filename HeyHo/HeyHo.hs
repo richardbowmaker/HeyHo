7c4acd366904e267c64424803e34e207ab8a7a32
richardbowmaker <rjb.accounts@live.co.uk>
2018-01-05 18:58:37 +0000

    Got Scintilla to load and resize in main window client area

--- HeyHo/HeyHo.hs
+++ HeyHo/HeyHo.hs
@@ -6,15 +6,18 @@ import Graphics.UI.WXCore
 import Foreign
 import Foreign.C.Types
 import Foreign.C.String
+
 import System.Win32.DLL
 import System.Win32.Types
 import Graphics.Win32.Window
 import Graphics.Win32.GDI.Types
 import Graphics.Win32.Message
 
+
 foreign import ccall "libc.h exp" c_exp :: Double -> Double
 foreign import ccall "math.h sin" c_sin :: CDouble -> CDouble
-foreign import ccall unsafe "fnTheDll" c_fnTheDll :: HWND -> IO (CInt)
+--foreign import ccall unsafe "fnTheDll" c_fnTheDll :: HWND -> IO (CInt)
+foreign import ccall unsafe "SetWindowPos" c_SetWindowPos :: HWND -> HWND -> Int -> Int -> Int -> Int -> Word32 -> IO (Int)
 
 main = start mainGUI
 
@@ -24,41 +27,65 @@ mainGUI :: IO ()
 mainGUI = do 
 
 
-    f <- frameFixed [] 
+
+
+
+    f <- frame [] 
+    p <- panel  f []
     
-    set f [ text := "HeyHo", 
-            bgcolor     := white, 
-            layout      := space 400 400
-          ]
+    set f [ text:= "HeyHo", bgcolor := white]
            
-    test <- button f [text := "Test "]
+    -- b  <- button f [text := "Test "]   
+    -- set test [on command := cmdTest f test]
+
+    -- panel fills frame client area
+    set p [ bgcolor := red]
+
+
+    -- 
+--    set f [ layout := container p $ minsize (sz 200 200) $ fill $ label "x:"]
+    set f [ layout := minsize (sz 400 400) $ fill $ widget p]
+--    windowSetSize f (Rect 0 0 500 500) wxSIZE_USE_EXISTING 
+    set f [ size := (Size 500 500)]
     
-    set test [on command := cmdTest f test]
     
-    return ()
-    
-{-
-loadLibrary' :: String -> IO Int
-loadLibrary' lib = do 
-    cstr <- newCString lib
-    hinstance <- c_loadLibrary' cstr
-    return (fromIntegral hinstance)
+{-   
+    hwnd <- windowGetHandle p 
+    c_fnTheDll hwnd   
+-}
+
+    hwnd <- windowGetHandle p   
+    hinstance <- loadLibrary "D:\\_Rick's\\haskell\\HeyHo\\SciLexer64.dll"
+    hwnd1 <- createWindow (mkClassName "Scintilla") "Source" (wS_CHILD + wS_VSCROLL + wS_HSCROLL + wS_CLIPCHILDREN) 
+        (Just 0)  (Just 0)  (Just 500)  (Just 500) (Just hwnd) Nothing hinstance winClose                    
+    showWindow hwnd1 sW_SHOW
+        
+   
+    set p [on resize := panelResize p hwnd1]
 
     
-marshallTypes :: String -> Int -> Double -> IO ()
-marshallTypes s n d = do 
-    cstr <- newCString s 
-    c_marshallTypes cstr n d
     return ()
     
+
+panelResize :: Panel() -> HWND -> IO ()
+panelResize p hwnd = do
+    (Size x y) <- get p size
+    c_SetWindowPos hwnd hWND_TOP 0 0 x y sWP_NOMOVE
+    return ()
+
+{-   
+cmdTest :: Panel () -> Button () -> IO ()
+cmdTest p b = do
+    set b [ text := "Clicked! "]
+    hwnd <- windowGetHandle p 
+    c_fnTheDll hwnd
+    return ()
 -}
     
-cmdTest :: Frame () -> Button () -> IO ()
-cmdTest f b = do
-    set b [ text := "Clicked! "]
-    hwnd <- windowGetHandle f 
-    c_fnTheDll hwnd   
-    return ()
+winClose :: HWND ->  WindowMessage -> WPARAM -> LPARAM -> IO LRESULT
+winClose _ _ _ _ = do 
+    return (0)
+        
     
     
     
